aed967dd145a6e0c337cb9c41c104584e0304102
Chhouk Titpanhara <titpanhara@gmail.com>
2019-09-03 20:12:53 +0700

    fix intro slider

--- App.js
+++ App.js
@@ -1,13 +1,8 @@
 import React from 'react';
-import { StyleSheet, View, Text, Image } from 'react-native';
+import { StyleSheet, View, Text, Image, TouchableOpacity } from 'react-native';
 import AppIntroSlider from 'react-native-app-intro-slider';
 
 const styles = StyleSheet.create({
-  slide:{
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center'
-  },  
   image: {
     width: 320,
     height: 320,
@@ -25,25 +20,54 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     marginBottom: 16,
   },
+  container: {
+    paddingTop: 80,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  fruitOne: {
+    width: 150,
+    height: 150,
+    resizeMode: 'contain'
+  },
+  header: {
+    paddingTop: 50,
+    fontSize: 28,
+    fontWeight: "bold",
+  },
+  paragraph: {
+    textAlign: 'center',
+    paddingHorizontal: 20,
+    paddingTop: 30
+  },
+  mainContainer: {
+    alignItems: 'center'
+  }
 });
 
 const slides = [
   {
     key: '1',
-    title: 'Title 1',
-    text: 'Description.\nSay something cool',
+    title: 'Explore',
+    text:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    image: require('./assets/Group49.png'),
     backgroundColor: '#59b2ab',
   },
   {
     key: '2',
-    title: 'Title 2',
-    text: 'Other cool stuff',
+    title: 'Make',
+    text:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    image: require('./assets/Group50.png'),
     backgroundColor: '#febe29',
   },
   {
     key: '3',
-    title: 'Rocket guy',
-    text: 'I\'m already out of descriptions\n\nLorem ipsum bla bla bla',
+    title: 'Share',
+    text:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    image: require('./assets/Group51.png'),
     backgroundColor: '#22bcb5',
   }
 ];
@@ -54,10 +78,23 @@ export default class App extends React.Component {
   }
   _renderItem = ({ item }) => {
     return (
-      <View style={styles.slide}>
-        <Text style={styles.title}>{item.title}</Text>
-        <Image source={item.image} />
-        <Text style={styles.text}>{item.text}</Text>
+      <View style={styles.container}>
+        <View style={
+          {
+            backgroundColor: item.backgroundColor,
+            width: 270,
+            height: 270,
+            borderRadius: 270/2,
+            alignItems: 'center',
+            justifyContent: 'center',
+          }
+        }>
+          <Image style={styles.fruitOne} source={item.image} />
+        </View>
+        <View style={styles.mainContainer}>
+          <Text style={ styles.header }>{ item.title }</Text>
+          <Text style={ styles.paragraph } ellipsizeMode="middle">{ item.text }</Text>
+        </View>
       </View>
     );
   }
@@ -70,12 +107,15 @@ export default class App extends React.Component {
     if (this.state.showRealApp) {
       return <App />;
     } else {
-      return <AppIntroSlider 
-        renderItem={this._renderItem} 
-        slides={slides} 
+      return <AppIntroSlider
+        renderItem={this._renderItem}
+        slides={slides}
         onDone={this._onDone}
-        dotStyle={{ backgroundColor: 'rgba(0,0,0,0.2)' }}
-        activeDotStyle={{ backgroundColor: 'rgba(102,153,255,0.2)' }}
+        dotStyle={{ backgroundColor: 'rgba(0,0,0,0.2)', marginBottom: 200 }}
+        activeDotStyle={{ backgroundColor: 'rgba(102,153,255,0.2)', marginBottom: 200 }}
+        bottomButton
+        buttonStyle={{ backgroundColor: "#1abc9c" }}
+        nextLabel="Get Started"
       />;
     }
   }
